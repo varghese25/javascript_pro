202e6d367674ac4ce63feda504aaac2f72ff5fd8
varghese25 <vbpoulose@gmail.com>
2025-01-22 14:03:47 -0500

    JS Day3 Prac

--- logic/script.js
+++ logic/script.js
@@ -11,9 +11,22 @@
         // console.log("score is " + score); // print the value of the variable in the console is 10 "score is " inside the double quotes is string;
         
       
-      //Exercise 1
-        let amount = 1000;
-      console.log("Your bill amout is Rs." + amount);
+      // //Exercise 1
+      //   let amount = 1000;
+      // console.log("Your bill amout is Rs." + amount);
 
     // const pi = 3.14; // constant variable cannot be changed 
     // pi = 3 
+
+
+    // Day 3
+    // // USER Input
+    // let name = prompt("Enter your name..?"); // prompt is a function that takes input from the user
+    // console.log("Hello" , name); // concatenation can be done using comma(,).
+
+    // let ticket = prompt("How manu tickets do you want..?"); // data stored in the variable ticket 
+    // console.log("You want pay",Number(ticket)*20 ); // Number() is a function that converts string to number
+
+    let a = prompt("Enter a number..?");
+    console.log(Number(a)+10);
+    //----------------------------------------------------------------
