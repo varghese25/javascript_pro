4f5353dcf596c7a710d8dc4fa69068d786624e7c
Varghese Baby <Vbpoulose@gmail.com>
2025-09-17 10:47:10 -0400

    Js: Arrays are add + convert to string

---
+++ InterView/Prac4.js
@@ -0,0 +1,6 @@
+console.log([1, 2, 3] + [4, 5, 6]);
+//When Array are added with +, they are converterd to string:
+// [1,2,3].toString() -> "1,2,3"
+// [4,5,6].toString() -> "4,5,6"
+//Then the two strings are concatenated.
+//So the result is "1,2,34,5,6" String.
